d7ac88d2250d4c118d47180ddae5509cf4cf34a3
zdeslav <zdeslav.vojkovic@gmail.com>
2014-04-01 12:34:29 +0200

    tweaks

--- metrics/stdafx.cpp
+++ metrics/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// metro.pch will be the pre-compiled header
+// metrics.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
